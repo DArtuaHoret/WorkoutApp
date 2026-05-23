0e0d7e8243d776aa34f01f8a3f5de053e431f5eb
qquweoom <anzhalika.kupryianava@gmail.com>
2026-05-23 15:01:00 +0200

    build(deps): add material-icons-extended to version catalog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -74,4 +74,6 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+    implementation(libs.icons.extended)
+    implementation(libs.compose.material.icons.extended)
 }
